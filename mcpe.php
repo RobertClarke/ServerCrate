a556a9a4bcb707834cadfe28cac59d05774a43bb
RobertClarke <robert@rjfc.net>
2013-09-06 08:00:44 -0700

    Update mcpe.php

--- mcpe.php
+++ mcpe.php
@@ -1,5 +1,5 @@
 <?php $page["page"] = "minecraft"; $page["title"] = "Minecraft PE Hosting"; include("./incl/header.php"); ?>
-    <h1 class="imgTitle minecraft">Minecraft PE Server Hosting with PocketMine</h1>
+    <h1 class="imgTitle minecraft">START YOUR OWN MINECRAFT PE SERVER TODAY</h1>
     <div id="content">
         <div id="contentContainer">
             <div id="web-plans" class="minecraft">
@@ -78,12 +78,12 @@
             </div><div class="clear"></div>
             <hr />
             <div class="one-third">
-                <h2 class="icon computer">High Quality Hardware</h2>
-                <p>Our servers are built with only the latest E3v2 Intel processors, high quality Supermicro server grade motherboards, and high performance Samsung 840 solid state drives to ensure speed and reliability for each customer.</p>
+                <h2 class="icon computer">FAST SETUP</h2>
+                <p>Upon ordering a Minecraft server with ServerCrate, your server is setup instantly, and your own server IP address is sent right to your email address, so you can right into breaking those blocks.</p>
             </div>
             <div class="one-third">
-                <h2 class="icon lamp">Top Tier Network</h2>
-                <p>These Minecraft PE servers are the first of their kind. ServerCrate is the only hosting company with an included control panel for managing your Minecraft PE server from the internet.</p>
+                <h2 class="icon lamp">Control panel</h2>
+                <p>We use a modified vesion of our powerful Minecraft control panel, making it easy to manage your Minecraft PE server. You can start/stop the server, and upload files with ease./p>
             </div>
             <div class="one-third last">
                 <h2 class="icon thumbup">Premium Support</h2>
