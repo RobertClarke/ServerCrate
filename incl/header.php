d5ab8c978d1ab09434454b1e04fa5a303767893f
RobertClarke <robert@rjfc.net>
2013-09-21 14:02:29 -0700

    Update header.php

--- incl/header.php
+++ incl/header.php
@@ -32,6 +32,19 @@
           w.attachEvent("onload", gs);
       })(window);
     </script>
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-8521263-19']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body id="<?php echo $page["page"]; ?>">
     <div id="header">
