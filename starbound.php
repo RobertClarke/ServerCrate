45a2de2c557f64567aab59d840d9e831d1be0585
RobertClarke <robert@rjfc.net>
2013-12-06 08:23:40 -0800

    Starbound initial

---
+++ starbound.php
@@ -0,0 +1,57 @@
+<?php $page["page"] = "dedicated"; $page["title"] = "Dedicated Server Plans"; include("./incl/header.php"); ?>
+    <h1 class="imgTitle dedicated">Powerful &amp; Reliable Dedicated Servers</h1>
+    <div id="content">
+        <div id="contentContainer">
+            <h2>Unleash The True Power of Dedicated Servers</h2>
+            <p class="nospace justify">When power is a necessity, look no further than our dedicated server line. We offer custom dedicated server solutions with plenty of RAM, and bandwidth. Each server is hand build with Supermicro server grade motherboards, and enterprise-grade drives and RAM. ServerCrate management options are available for your server if you need a hand getting up and running.</p>
+            <hr />
+            <div id="web-plans" class="dedi">
+                <div class="plan first cpu">
+                    <h3>Starbound VPS</h3>
+                    <ul>
+                        <li>Starbound Service</li>
+                    </ul>
+                </div>
+                <div class="plan ram">
+                    <h3>RAM</h3>
+                    <ul>
+                        <li>1GB</li>
+                    </ul>
+                </div>
+                <div class="plan storage">
+                    <h3>Storage</h3>
+                    <ul>
+                        <li>10GB SSD</li>
+                    </ul>
+                </div>
+                <div class="plan traffic">
+                    <h3>Traffic</h3>
+                    <ul>
+                        <li>1TB</li>
+                    </ul>
+                </div>
+                <div class="plan last starting">
+                    <h3>Price</h3>
+                    <ul>
+                        <li><a href="https://billing.servercrate.com/cart.php?a=add&pid=39" class="button buy">$12/mo - Pre-Order Now</a></li>
+                    </ul>
+                </div>
+            </div><div class="clear"></div>
+            <hr />
+            <div class="one-third">
+                <h2 class="icon computer">High Quality Hardware</h2>
+                <p>Our servers are built with only the latest Intel processors, high quality Supermicro server grade motherboards, and high performance Samsung and Intel drives to ensure speed and reliability for each customer.</p>
+            </div>
+            <div class="one-third">
+                <h2 class="icon network">Top Tier Network</h2>
+                <p>Our servers are located in Dallas, TX inside a state-of-the-art, secure data center facility and features premium bandwidth connections to multiple bandwidth providers for maximum reliability and speed.</p>
+            </div>
+            <div class="one-third last">
+                <h2 class="icon thumbup">Premium Support</h2>
+                <p>We guarantee premium support, period. If any issues are to arise with any of your hosted services, you'll always be assisted by a dedicated ServerCrate team member who will quickly resolve any issues.</p>
+            </div>
+            <hr class="nospace" /><div class="clear"></div>
+        <div class="clear"></div>
+        </div>
+    </div>
+<?php include("./incl/footer.php"); ?>
