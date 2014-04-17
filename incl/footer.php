89b5ec6039d71c22d0d0ec0df6e5a0133974cbe4
RobertClarke <robert@rjfc.net>
2014-04-16 21:44:14 -0700

    Link fixes

--- incl/footer.php
+++ incl/footer.php
@@ -3,33 +3,33 @@
             <div class="col">
                 <h2>ServerCrate</h2>
                 <ul>
-                    <li><a href="./index.php">Home</a></li>
-                    <li><a href="./why-us.php">Why Us?</a></li>
-                    <li><a href="./plans.php">VPS Plans</a></li>
-                    <li><a href="./web-hosting.php">Web Hosting Plans</a></li>
-                    <li><a href="./minecraft.php">Minecraft Plans</a></li>
-                    <li><a href="./features.php">Hosting Features</a></li>
-                    <li><a href="./company.php">Our Company</a></li>
+                    <li><a href="./">Home</a></li>
+                    <li><a href="./why-us">Why Us?</a></li>
+                    <li><a href="./plans">VPS Plans</a></li>
+                    <li><a href="./web-hosting">Web Hosting Plans</a></li>
+                    <li><a href="./minecraft">Minecraft Plans</a></li>
+                    <li><a href="./features">Hosting Features</a></li>
+                    <li><a href="./company">Our Company</a></li>
                 </ul>
             </div>
             <div class="col">
                 <h2>Customer Portal</h2>
                 <ul>
-                    <li><a href="http://servercrate.com/billing">Portal Home</a></li>
-                    <li><a href="http://servercrate.com/billing/clientarea.php?action=products">My Services</a></li>
-                    <li><a href="http://servercrate.com/billing/cart.php">Shopping Cart</a></li>
-                    <li><a href="http://servercrate.com/billing/clientarea.php?action=invoices">Invoices</a></li>
+                    <li><a href="https://billing.servercrate.com/">Portal Home</a></li>
+                    <li><a href="https://billing.servercrate.com/clientarea.php?action=products">My Services</a></li>
+                    <li><a href="https://billing.servercrate.com/cart.php">Shopping Cart</a></li>
+                    <li><a href="https://billing.servercrate.com/clientarea.php?action=invoices">Invoices</a></li>
                     <li><a href="https://billing.servercrate.com/clientarea.php?action=addfunds">Add Funds</a></li>
-                    <li><a href="http://servercrate.com/billing/supporttickets.php">Support Tickets</a></li>
-                    <li><a href="http://billing.servercrate.com/knowledgebase.php">Knowledgebase</a></li>
+                    <li><a href="https://billing.servercrate.com/supporttickets.php">Support Tickets</a></li>
+                    <li><a href="https://billing.servercrate.com/knowledgebase.php">Knowledgebase</a></li>
                 </ul>
             </div>
             <div class="col">
                 <h2>Customer Tools</h2>
                 <ul>
-                    <li><a href="http://servercrate.com/billing">Customer Portal</a></li>
+                    <li><a href="https://billing.servercrate.com/">Customer Portal</a></li>
                     <li><a href="http://vps.servercrate.com/">VPS Control Panel</a></li>
-                    <li><a href="http://dalweb1.servercrate.com/cpanel">Web Hosting Control Panel</a></li>
+                    <li><a href="http://web.servercrate.com/cpanel">Web Hosting Control Panel</a></li>
                 </ul>
             </div>
             <div class="col last">
